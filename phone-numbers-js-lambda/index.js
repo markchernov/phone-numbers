a214c62b4159417e57beffb14acba186bf3aa2cb
markchernov <mark.chernov@gmail.com>
2020-03-13 21:07:42 -0400

    move cache outside handler

--- phone-numbers-js-lambda/index.js
+++ phone-numbers-js-lambda/index.js
@@ -1,42 +1,40 @@
-exports.handler = async event => {
-  const cache = {};
-  const keypad = [
-    ["0"],
-    ["1"],
-    ["A", "B", "C"],
-    ["D", "E", "F"],
-    ["G", "H", "I"],
-    ["J", "K", "L"],
-    ["M", "N", "O"],
-    ["P", "Q", "R", "S"],
-    ["T", "U", "V"],
-    ["W", "X", "Y", "Z"]
-  ];
+const cache = {};
+const keypad = [
+  ["0"],
+  ["1"],
+  ["A", "B", "C"],
+  ["D", "E", "F"],
+  ["G", "H", "I"],
+  ["J", "K", "L"],
+  ["M", "N", "O"],
+  ["P", "Q", "R", "S"],
+  ["T", "U", "V"],
+  ["W", "X", "Y", "Z"]
+];
+const calculateCombinations = (phoneNumber, keypad, cache) => {
+  const input = [...phoneNumber];
+  let output = [];
 
-  const calculateCombinations = (phoneNumber, keypad, cache) => {
-    const input = [...phoneNumber];
-    let output = [];
+  for (let ch of keypad[input[0]]) {
+    output.push(ch);
+  }
 
-    for (let ch of keypad[input[0]]) {
-      output.push(ch);
-    }
+  for (let i = 1; i < input.length; i++) {
+    const prevList = [...output];
+    output = [];
 
-    for (let i = 1; i < input.length; i++) {
-      const prevList = [...output];
-      output = [];
-
-      for (let ch of keypad[input[i]]) {
-        for (let s of prevList) {
-          output.push(s + ch);
-        }
+    for (let ch of keypad[input[i]]) {
+      for (let s of prevList) {
+        output.push(s + ch);
       }
     }
-    cache[phoneNumber] = output;
-    return output;
-  };
+  }
+  cache[phoneNumber] = output;
+  return output;
+};
 
+exports.handler = async event => {
   const request = event.body ? JSON.parse(event.body) : event;
-
   const combinations = cache[request.phoneNumber]
     ? cache[request.phoneNumber]
     : calculateCombinations(request.phoneNumber, keypad, cache);
